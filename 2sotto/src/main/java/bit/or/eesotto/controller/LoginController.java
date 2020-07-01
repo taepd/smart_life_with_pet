35e7af6c5bb91c1f627fb40e4127cf04d0229aa1
taepd <taepd1@gmail.com>
2020-07-01 20:08:27 +0900

    프로젝트 기본 세팅

---
+++ 2sotto/src/main/java/bit/or/eesotto/controller/LoginController.java
@@ -0,0 +1,60 @@
+package bit.or.eesotto.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import bit.or.eesotto.dto.User;
+import bit.or.eesotto.service.LoginService;
+
+@Controller
+public class LoginController {
+//	private SqlSession sqlsession;
+//
+//	@Autowired
+//	public void setSqlsession(SqlSession sqlsession) {
+//		this.sqlsession = sqlsession;
+//	}
+
+	@Autowired
+	LoginService ls;
+
+	
+	// 로그인 view
+	@RequestMapping(value = "login.bit", method = RequestMethod.GET) 
+	public String login() { return "index"; }
+	 
+
+	// 일반 로그인 처리
+	@RequestMapping(value="nomalLogin.bit", method = RequestMethod.POST)
+	public String nomalLogin(String userid, String pwd, HttpSession session, Model model) {
+		
+		User user = null;
+		
+		user = ls.normalLogin(userid);
+		
+		String msg = "";
+		String url = "";
+
+		if (user != null && userid.equals(user.getUserid()) && pwd.equals(user.getPwd())) {
+			session.setAttribute("userid", userid);
+			msg = "로그인 성공";
+			url = "Main.bit";
+
+		} else {
+			msg = "로그인 실패";
+			url = "./index.html";
+		}
+
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+
+		return "redirect";
+	}
+
+}
---
+++ 2sotto/src/main/java/bit/or/eesotto/dao/UserDao.java
@@ -0,0 +1,36 @@
+package bit.or.eesotto.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+
+import bit.or.eesotto.dto.User;
+
+
+public interface UserDao {
+
+	// 일반 로그인
+	@Select("select * from user where userid = #{userid}")
+	public User getUser(@Param("userid") String userid);
+
+//	// SNS로그인 (가입한 소셜까지 확인)
+//	@Select("select * from users where uEmail = #{uEmail}")
+//	public User snsLogin(@Param("uEmail") String uEmail);
+//
+//	// 자동 로그인 = 로그인 유지
+//	@Update("update users set session_key = #{session_key}, session_limit = #{session_limit} where uEmail = #{uEmail}")
+//	public void keepLogin(User user);
+//
+//	// 자동 로그인을위한 세션키 검증
+//	@Select("select * from users where session_key = #{cookie_key}")
+//	public User checkLoginBefore(@Param("cookie_key") String cookie_key);
+//
+//	// 가입 이메일 찾기
+//	@Select("select uEmail from users where uPhone = #{uPhone}")
+//	public User findRegisterEmail(User user);
+//
+//	// 비밀번호 재설정
+//	@Update("update users set uPassword = #{uPassword} where uEmail = #{uEmail}")
+//	public int resetPassword(User user);
+
+}

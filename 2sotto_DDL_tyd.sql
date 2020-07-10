2d8c7535ff53ac877e834df9135a4ac4a1404493
taepd <taepd1@gmail.com>
2020-07-11 00:43:41 +0900

    MYSQL DB 기본쿼리 정리

--- 2sotto_DDL_tyd.sql
+++ 2sotto_DDL_tyd.sql
@@ -6,8 +6,8 @@ CREATE TABLE USER (
 	LOC      VARCHAR(300) NOT NULL, -- 위치정보
 	CPNUMBER VARCHAR(40)  NOT NULL, -- 휴대전화번호
 	ENABLED  VARCHAR(4)   NOT NULL, -- ENABLED
-	UIMG  VARCHAR(500) NULL,     -- 프로필 사진
-	RTIME    VARCHAR(40)  NOT NULL, -- 가입일시
+	UIMG     VARCHAR(500) NULL,     -- 프로필 사진
+	RTIME    TIMESTAMP    NOT NULL, -- 가입일시
 	LAT      VARCHAR(40)  NULL,     -- 위도
 	LOT      VARCHAR(40)  NULL,     -- 경도
 	POINT    INT          NULL      -- 포인트
@@ -27,7 +27,7 @@ CREATE TABLE QNAREPLY (
 	ID      VARCHAR(20)  NOT NULL, -- ID
 	QAINDEX INT          NOT NULL, -- 글번호
 	CONTENT VARCHAR(500) NOT NULL, -- 내용
-	QARTIME VARCHAR(40)  NOT NULL  -- 등록시간
+	QARTIME TIMESTAMP  NOT NULL  -- 등록시간
 );
 
 -- 질의응답댓글
@@ -47,11 +47,11 @@ CREATE TABLE PET (
 	PETNAME   VARCHAR(20)  NOT NULL, -- 반려동물명
 	SEX       VARCHAR(20)  NOT NULL, -- 성별
 	AGE       INT          NOT NULL, -- 나이
-	SIZE      VARCHAR(20)         NOT NULL,     -- 크기
-	WEIGHT    INT          NULL,     -- 몸무게
-	HLENGTH    INT          NULL,     -- 털길이
+	SIZE      VARCHAR(20)  NOT NULL, -- 크기
+	WEIGHT    VARCHAR(40)  NULL,     -- 몸무게
+	HLENGTH   VARCHAR(40)  NULL,     -- 털길이
 	NSTATE    VARCHAR(4)   NOT NULL, -- 중성화여부
-	PETIMG   VARCHAR(500) NULL,     -- 사진
+	PETIMG    VARCHAR(500) NULL,     -- 사진
 	MEMO      VARCHAR(200) NULL      -- 메모
 );
 
@@ -73,11 +73,11 @@ ALTER TABLE MAINCATEGORY
 
 -- 접종/검사 
 CREATE TABLE INSPECTION (
-	IINDEX   INT    PRIMARY KEY AUTO_INCREMENT, -- 글번호
+	IINDEX   INT          PRIMARY KEY AUTO_INCREMENT, -- 글번호
 	PETINDEX INT          NOT NULL, -- 동물식별번호
 	USERID   VARCHAR(40)  NOT NULL, -- USERID
 	LIST     VARCHAR(100) NOT NULL, -- 항목
-	VDATE    DATE         NULL,     -- 접종예정일
+	VDATE    TIMESTAMP    NULL,     -- 접종예정일
 	VSTATE   VARCHAR(4)   NULL,     -- 접종여부
 	VCOUNT   INT          NULL      -- 잔여접종횟수
 );
@@ -86,10 +86,10 @@ CREATE TABLE INSPECTION (
 
 -- 동물 병원 이용 기록
 CREATE TABLE MRECORD (
-	MINDEX   INT        PRIMARY KEY AUTO_INCREMENT, -- 글번호
+	MINDEX   INT         PRIMARY KEY AUTO_INCREMENT, -- 글번호
 	PETINDEX INT         NOT NULL, -- 동물식별번호
 	USERID   VARCHAR(40) NOT NULL, -- USERID
-	VDATE    DATE        NOT NULL, -- 방문날짜
+	VDATE    TIMESTAMP   NOT NULL, -- 방문날짜
 	VREASON  VARCHAR(60) NOT NULL, -- 방문 사유
 	HNAME    VARCHAR(20) NOT NULL  -- 병원명
 );
@@ -97,30 +97,30 @@ CREATE TABLE MRECORD (
 
 
 -- 게시글
-CREATE TABLE POST (
-	BDINDEX  INT           PRIMARY KEY AUTO_INCREMENT, -- 글번호
+CREATE TABLE BLOG (
+	BINDEX  INT            PRIMARY KEY AUTO_INCREMENT, -- 글번호
 	PETINDEX INT           NOT NULL, -- 동물식별번호
 	USERID   VARCHAR(40)   NOT NULL, -- USERID
 	BLIKE    INT           NULL,     -- 추천수(보류)
 	TITLE    VARCHAR(100)  NOT NULL, -- 제목
 	CONTENT  VARCHAR(4000) NOT NULL, -- 내용
-	RTIME    DATE          NOT NULL, -- 등록시간
+	RTIME    TIMESTAMP     NOT NULL, -- 등록시간
 	DELSTATE VARCHAR(4)    NOT NULL, -- 삭제유무
 	COUNT    INT           NOT NULL, -- 조회수
-	PIMG      VARCHAR(500)  NULL      -- 이미지
+	PIMG     VARCHAR(500) NULL       -- 이미지
 );
 
 
 
 -- 댓글
-CREATE TABLE COMMENT (
-	RPINDEX INT          PRIMARY KEY AUTO_INCREMENT, -- 댓글번호
-	BDINDEX INT          NOT NULL, -- 글번호
+CREATE TABLE BLOGCOMMENT (
+	BCINDEX INT          PRIMARY KEY AUTO_INCREMENT, -- 댓글번호
+	BINDEX  INT          NOT NULL, -- 글번호
 	USERID  VARCHAR(40)  NOT NULL, -- USERID
-	RPLIKE  INT          NULL,     -- 추천수(보류)
+	BCLIKE  INT          NULL,     -- 추천수(보류)
 	CONTENT VARCHAR(400) NOT NULL, -- 내용
 	SCSTATE VARCHAR(4)   NOT NULL, -- 비밀유무
-	RTIME   VARCHAR(60)  NOT NULL, -- 등록시간
+	RTIME   TIMESTAMP    NOT NULL, -- 등록시간
 	REFER   INT          NOT NULL, -- refer
 	DEPTH   INT          NOT NULL, -- depth
 	STEP    INT          NOT NULL  -- step
@@ -132,10 +132,10 @@ CREATE TABLE COMMENT (
 CREATE TABLE MESSAGE (
 	MSINDEX   INT          PRIMARY KEY AUTO_INCREMENT, -- 쪽지번호
 	RUSERID   VARCHAR(40)  NOT NULL, -- 받은사람ID
-	SUSERID2  VARCHAR(40)  NOT NULL, -- 보낸사람ID
-	TITLE     VARCHAR(100) NOT NULL, -- 쪽지내용
-	QATIME    VARCHAR(40)  NOT NULL, -- 보낸일시
-	ID        VARCHAR(20)  NOT NULL, -- 읽은일시
+	SUSERID   VARCHAR(40)  NOT NULL, -- 보낸사람ID
+	CONTENT   VARCHAR(100) NOT NULL, -- 쪽지내용
+	SENDTIME  TIMESTAMP    NOT NULL, -- 보낸일시
+	READTIME  TIMESTAMP    NOT NULL, -- 읽은일시
 	READSTATE VARCHAR(4)   NULL      -- 읽음여부
 );
 
@@ -143,14 +143,14 @@ CREATE TABLE MESSAGE (
 
 -- 일정
 CREATE TABLE SCHEDULE (
-	SINDEX          INT          PRIMARY KEY AUTO_INCREMENT, -- 글번호
+	SINDEX      INT          PRIMARY KEY AUTO_INCREMENT, -- 글번호
 	PETINDEX    INT          NOT NULL, -- 동물식별번호
 	USERID      VARCHAR(40)  NOT NULL, -- USERID
 	TITLE       VARCHAR(60)  NOT NULL, -- 제목
 	CONTENT     VARCHAR(400) NOT NULL, -- 내용
 	IS_COMPLETE VARCHAR(4)   NULL,     -- 완료여부
-	BEGIN_DATE  DATE         NOT NULL, -- 시작날짜
-	END_DATE    DATE         NULL,     -- 마감날짜
+	BEGIN_DATE  TIMESTAMP    NOT NULL, -- 시작날짜
+	END_DATE    TIMESTAMP    NULL,     -- 마감날짜
 	CYCLE       TIME         NOT NULL, -- 주기
 	ADNCDNOTI   VARCHAR(40)  NULL      -- 미리알림
 );
@@ -159,17 +159,17 @@ CREATE TABLE SCHEDULE (
 
 -- 추천
 CREATE TABLE RECOM (
-	LIKEINDEX INT      PRIMARY KEY AUTO_INCREMENT, -- 구분번호
+	LIKEINDEX INT         PRIMARY KEY AUTO_INCREMENT, -- 구분번호
 	USERID    VARCHAR(40) NOT NULL, -- USERID
-	BDINDEX   INT         NOT NULL, -- 글번호
-	RPINDEX   INT         NOT NULL  -- 댓글번호
+	BINDEX    INT         NOT NULL, -- 글번호
+	BCINDEX   INT         NOT NULL  -- 댓글번호
 );
 
 
 
 -- 질의응답
 CREATE TABLE QNA (
-	QAINDEX  INT   PRIMARY KEY AUTO_INCREMENT, -- 글번호
+	QAINDEX  INT          PRIMARY KEY AUTO_INCREMENT, -- 글번호
 	USERID   VARCHAR(40)  NULL,     -- USERID
 	TITLE    VARCHAR(100) NOT NULL, -- 제목
 	QATIME   VARCHAR(40)  NOT NULL, -- 등록시간
@@ -208,12 +208,25 @@ CREATE TABLE DONATE (
 	TITLE   VARCHAR(120)  NOT NULL, -- 제목
 	CONTENT VARCHAR(4000) NOT NULL, -- 내용
 	DOBJECT VARCHAR(60)   NOT NULL, -- 기부대상
-	RTIME   DATE          NOT NULL, -- 등록일시
-	CTIME   DATE          NULL,
-    	DIMG    VARCHAR(500)  NULL
+	RTIME   TIMESTAMP     NOT NULL, -- 등록일시
+	CTIME   TIMESTAMP     NULL,
+	GCOLL   INT           NOT NULL, -- 목표기부금
+	CCOLL   INT           NOT NULL, -- 현재기부금
+	DIMG    VARCHAR(500)  NULL
 		
 );
 
+-- 기부댓글
+CREATE TABLE DONATECOMMENT (
+	DCINDEX INT          PRIMARY KEY AUTO_INCREMENT, -- 댓글번호
+	DINDEX  INT          NOT NULL, -- 기부글번호
+	USERID  VARCHAR(40)  NOT NULL, -- USERID
+	RPLIKE  INT          NULL,     -- 추천수(보류)
+	CONTENT VARCHAR(400) NOT NULL, -- 내용
+	SCSTATE VARCHAR(4)   NOT NULL, -- 비밀유무
+	RTIME   TIMESTAMP    NOT NULL -- 등록시간
+	
+);
 
 
 -- 결제(돈내고 한 것만)
@@ -232,7 +245,7 @@ CREATE TABLE PAY (
 CREATE TABLE BLIKE (
 	BMINDEX INT   PRIMARY KEY AUTO_INCREMENT, -- 즐겨찾기번호
 	USERID  VARCHAR(40) NOT NULL, -- USERID
-	BDINDEX INT         NOT NULL  -- 글번호
+	BINDEX  INT         NOT NULL  -- 글번호
 );
 
 
@@ -249,6 +262,7 @@ CREATE TABLE PETLIKE (
 -- 동물카테고리/소분류
 CREATE TABLE SUBCATEGORY (
 	SCATEGORY VARCHAR(20) NOT NULL, -- 소분류코드
+	MCATEGORY VARCHAR(20) NOT NULL, -- 대분류코드
 	SCANAME   VARCHAR(10) NULL      -- 소분류명
 );
 
@@ -267,7 +281,7 @@ CREATE TABLE POINT (
 	PTYPE   VARCHAR(4)  NULL,     -- 포인트 유형
 	PCOUNT  INT         NULL,     -- 포인트 수량
 	PAMOUNT INT         NULL,     -- 포인트 총량
-	PDATE   DATE         NULL      -- 포인트 일시
+	PDATE  TIMESTAMP    NULL      -- 포인트 일시
 );
 
 
@@ -284,7 +298,8 @@ ALTER TABLE QNAREPLY
 		)
 		REFERENCES QNA ( -- 질의응답
 			QAINDEX -- 글번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 반려동물
 ALTER TABLE PET
@@ -295,7 +310,8 @@ ALTER TABLE PET
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 반려동물
 ALTER TABLE PET
@@ -306,7 +322,8 @@ ALTER TABLE PET
 		)
 		REFERENCES MAINCATEGORY ( -- 동물카테고리/대분류
 			MCATEGORY -- 대분류코드
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 반려동물
 ALTER TABLE PET
@@ -317,7 +334,20 @@ ALTER TABLE PET
 		)
 		REFERENCES SUBCATEGORY ( -- 동물카테고리/소분류
 			SCATEGORY -- 소분류코드
-		);
+		)
+		ON DELETE CASCADE;
+
+-- 소분류
+ALTER TABLE SUBCATEGORY
+	ADD
+		CONSTRAINT FK_MAINCATEGORY_TO_SUBCATEGORY -- 
+		FOREIGN KEY (
+			MCATEGORY -- 대분류코드
+		)
+		REFERENCES MAINCATEGORY ( -- 대분류
+			MCATEGORY -- 대분류코드
+		)
+		ON DELETE CASCADE;
 
 -- 접종/검사 
 ALTER TABLE INSPECTION
@@ -328,7 +358,8 @@ ALTER TABLE INSPECTION
 		)
 		REFERENCES PET ( -- 반려동물
 			PETINDEX -- 동물식별번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 접종/검사 
 ALTER TABLE INSPECTION
@@ -339,7 +370,8 @@ ALTER TABLE INSPECTION
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 동물 병원 이용 기록
 ALTER TABLE MRECORD
@@ -350,7 +382,8 @@ ALTER TABLE MRECORD
 		)
 		REFERENCES PET ( -- 반려동물
 			PETINDEX -- 동물식별번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 동물 병원 이용 기록
 ALTER TABLE MRECORD
@@ -361,73 +394,80 @@ ALTER TABLE MRECORD
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 게시글
-ALTER TABLE POST
+ALTER TABLE BLOG
 	ADD
-		CONSTRAINT FK_PET_TO_POST -- 반려동물 -> 게시글
+		CONSTRAINT FK_PET_TO_BLOG -- 반려동물 -> 게시글
 		FOREIGN KEY (
 			PETINDEX -- 동물식별번호
 		)
 		REFERENCES PET ( -- 반려동물
 			PETINDEX -- 동물식별번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 게시글
-ALTER TABLE POST
+ALTER TABLE BLOG
 	ADD
-		CONSTRAINT FK_USER_TO_POST -- 회원 -> 게시글2
+		CONSTRAINT FK_USER_TO_BLOG -- 회원 -> 게시글2
 		FOREIGN KEY (
 			USERID -- USERID
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 댓글
-ALTER TABLE COMMENT
+ALTER TABLE BLOGCOMMENT
 	ADD
-		CONSTRAINT FK_USER_TO_COMMENT -- 회원 -> 댓글2
+		CONSTRAINT FK_USER_TO_BLOGCOMMENT -- 회원 -> 댓글2
 		FOREIGN KEY (
 			USERID -- USERID
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 댓글
-ALTER TABLE COMMENT
+ALTER TABLE BLOGCOMMENT
 	ADD
-		CONSTRAINT FK_POST_TO_COMMENT -- 게시글 -> 댓글2
+		CONSTRAINT FK_BLOG_TO_BLOGCOMMENT -- 게시글 -> 댓글2
 		FOREIGN KEY (
-			BDINDEX -- 글번호
+			BINDEX -- 글번호
 		)
-		REFERENCES POST ( -- 게시글
-			BDINDEX -- 글번호
-		);
+		REFERENCES BLOG ( -- 게시글
+			BINDEX -- 글번호
+		)
+		ON DELETE CASCADE;
 
 -- 쪽지
 ALTER TABLE MESSAGE
 	ADD
-		CONSTRAINT FK_USER_TO_MESSAGE2 -- 회원 -> 쪽지
+		CONSTRAINT FK_USER_TO_MESSAGE -- 회원 -> 쪽지
 		FOREIGN KEY (
 			RUSERID -- 받은사람ID
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 쪽지
 ALTER TABLE MESSAGE
 	ADD
-		CONSTRAINT FK_USER_TO_MESSAGE -- 회원 -> 쪽지2
+		CONSTRAINT FK_USER_TO_MESSAGE_2 -- 회원 -> 쪽지
 		FOREIGN KEY (
-			SUSERID2 -- 보낸사람ID
+			SUSERID -- 보낸사람ID
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 일정
 ALTER TABLE SCHEDULE
@@ -438,7 +478,8 @@ ALTER TABLE SCHEDULE
 		)
 		REFERENCES PET ( -- 반려동물
 			PETINDEX -- 동물식별번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 일정
 ALTER TABLE SCHEDULE
@@ -449,7 +490,8 @@ ALTER TABLE SCHEDULE
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 추천
 ALTER TABLE RECOM
@@ -460,29 +502,32 @@ ALTER TABLE RECOM
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 추천
 ALTER TABLE RECOM
 	ADD
-		CONSTRAINT FK_POST_TO_RECOM -- 게시글 -> 추천
+		CONSTRAINT FK_BLOG_TO_RECOM -- 게시글 -> 추천
 		FOREIGN KEY (
-			BDINDEX -- 글번호
+			BINDEX -- 글번호
 		)
-		REFERENCES POST ( -- 게시글
-			BDINDEX -- 글번호
-		);
+		REFERENCES BLOG ( -- 게시글
+			BINDEX -- 글번호
+		)
+		ON DELETE CASCADE;
 
 -- 추천
 ALTER TABLE RECOM
 	ADD
 		CONSTRAINT FK_COMMENT_TO_RECOM -- 댓글 -> 추천
 		FOREIGN KEY (
-			RPINDEX -- 댓글번호
+			BCINDEX -- 댓글번호
 		)
-		REFERENCES COMMENT ( -- 댓글
-			RPINDEX -- 댓글번호
-		);
+		REFERENCES BLOGCOMMENT ( -- 댓글
+			BCINDEX -- 댓글번호
+		)
+		ON DELETE CASCADE;
 
 -- 질의응답
 ALTER TABLE QNA
@@ -493,7 +538,8 @@ ALTER TABLE QNA
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- user_role
 ALTER TABLE USERROLE
@@ -504,10 +550,9 @@ ALTER TABLE USERROLE
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		
-        )
-        on delete cascade; 
-		
+		)
+		ON DELETE CASCADE;
+
 -- user_role
 ALTER TABLE USERROLE
 	ADD
@@ -517,7 +562,8 @@ ALTER TABLE USERROLE
 		)
 		REFERENCES ROLES ( -- roles
 			ROLEID -- ROLEID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 결제(돈내고 한 것만)
 ALTER TABLE PAY
@@ -528,7 +574,8 @@ ALTER TABLE PAY
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 결제(돈내고 한 것만)
 ALTER TABLE PAY
@@ -539,7 +586,8 @@ ALTER TABLE PAY
 		)
 		REFERENCES DONATE ( -- 기부게시판
 			DINDEX -- 기부글번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 즐겨찾기/게시글
 ALTER TABLE BLIKE
@@ -550,18 +598,20 @@ ALTER TABLE BLIKE
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 즐겨찾기/게시글
 ALTER TABLE BLIKE
 	ADD
-		CONSTRAINT FK_POST_TO_BLIKE -- 게시글 -> 즐겨찾기/게시글
+		CONSTRAINT FK_BLOG_TO_BLIKE -- 게시글 -> 즐겨찾기/게시글
 		FOREIGN KEY (
-			BDINDEX -- 글번호
+			BINDEX -- 글번호
 		)
-		REFERENCES POST ( -- 게시글
-			BDINDEX -- 글번호
-		);
+		REFERENCES BLOG ( -- 게시글
+			BINDEX -- 글번호
+		)
+		ON DELETE CASCADE;
 
 -- 즐겨찾기/반려동물
 ALTER TABLE PETLIKE
@@ -572,7 +622,8 @@ ALTER TABLE PETLIKE
 		)
 		REFERENCES PET ( -- 반려동물
 			PETINDEX -- 동물식별번호
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 즐겨찾기/반려동물
 ALTER TABLE PETLIKE
@@ -583,7 +634,8 @@ ALTER TABLE PETLIKE
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
 
 -- 포인트내역
 ALTER TABLE POINT
@@ -594,15 +646,42 @@ ALTER TABLE POINT
 		)
 		REFERENCES USER ( -- 회원
 			USERID -- USERID
-		);
+		)
+		ON DELETE CASCADE;
+
+
+-- 기부댓글/유저
+ALTER TABLE DONATECOMMENT
+	ADD
+		CONSTRAINT FK_USER_TO_DONATECOMMENT -- 회원 -> 즐겨찾기/게시글
+		FOREIGN KEY (
+			USERID -- USERID
+		)
+		REFERENCES USER ( -- 회원
+			USERID -- USERID
+		)
+		ON DELETE CASCADE;
+
+-- 기부댓글 후원글번호
+ALTER TABLE DONATECOMMENT
+	ADD
+		CONSTRAINT FK_DONATE_TO_DONATECOMMENT -- 
+		FOREIGN KEY (
+			DINDEX -- 글번호
+		)
+		REFERENCES DONATE ( -- 게시글
+			DINDEX -- 글번호
+		)
+		ON DELETE CASCADE;
         
-select * from user;
-select * from roles;
 
 
+drop trigger insert_userrole;
+
+-- 회원 가입 시 권한부여 해주는 트리거
 delimiter $$
 
-create trigger insert_member
+create trigger insert_userrole
 after insert on user
 for each row
 BEGIN
@@ -620,23 +699,49 @@ BEGIN
 END $$
 
 delimiter ;
+
 -- 트리거 생성을 하려면 루트계정에서 아래 명령 실행 (오류 1419 관련)
 SET Global log_bin_trust_function_creators='ON';
 
-select * from user;	
+-- DB 리셋 후 입력할 기본 데이터
+# roles테이블 권한 추가
+insert into roles values ('1', 'ROLE_ADMIN'),
+				         ('2', 'ROLE_USER');
+commit;
 
-desc user;
+# admin계정 추가(비번 1004)
+INSERT INTO user (USERID, NICK, PWD, LOC, CPNUMBER, ENABLED, rtime) 
+VALUES ('admin', 'admin', '$2a$10$kqMUFtQ5XY/CQSM8SgIdVuah6sEDXF3Vvi4yXaspzVj5qKwShCSUa', '1', '1', '1',now());
+# a 계정 추가(비번 1004)
+INSERT INTO user (USERID, NICK, PWD, LOC, CPNUMBER, ENABLED, rtime) 
+VALUES ('a', 'a', '$2a$10$kqMUFtQ5XY/CQSM8SgIdVuah6sEDXF3Vvi4yXaspzVj5qKwShCSUa', '1', '1', '1',now());
 
+commit;
 
+# 동물 대분류 카테고리 추가
+INSERT INTO `2sotto`.`maincategory` (`MCATEGORY`, `MCANAME`) VALUES ('1', '개'), ('2', '고양이');
+commit;
 
-select userid from user where userid = 'a@aa.aa';
+# 동물 소분류 카테고리 추가
+INSERT INTO `2sotto`.`subcategory` (`SCATEGORY`, `MCATEGORY`, `SCANAME`) 
+VALUES ('1', '1', '시츄'),('2', '1', '포메라니안'),('3', '1', '진돗개'),
+       ('4', '1', '치와와'), ('5', '2', '페르시안'),('6', '2', '러시안블루'),
+	   ('7', '2', '코리안숏헤어');
+commit;
 
-DELETE FROM user WHERE (`USERID` = 'TYD');
+-- 기본 입력 데이터 끝
+
+-- 타입 변경
+-- ALTER TABLE [TABLE명] MODIFY [컬럼명] [타입];
+
+-- 자동 증가(시퀀스) 초기화
+-- ALTER TABLE [TABLE명] AUTO_INCREMENT = [시작할 값];
 
 desc post;
 desc pet;
 desc maincategory;
 desc message;
+desc user;
 
 select * from roles;
 select * from userrole;
@@ -647,27 +752,5 @@ select * from pet;
 select * from maincategory;
 select * from subcategory; 
 
-insert into post (petindex, userid, blike, title, content, rtime, delstate, count) 
-values (1, 'a', 0, 'title', 'content', date_format(now(),'%Y-%m-%d %T'), '0', 0);
-
-insert into userrole (userid, roleid) values('bb', '1'),
-											('bb', '2');
--- 타입 변경
--- ALTER TABLE [TABLE명] MODIFY [컬럼명] [타입];
-
-alter table post modify rtime timestamp;
-
--- 자동 증가(시퀀스) 초기화
--- ALTER TABLE [TABLE명] AUTO_INCREMENT = [시작할 값];
-
-select count(*) from post where userid='a';
 
 
-select u.userid as username, r.rolename 
-										   from userrole u join roles r 
-										   on u.roleid = r.roleid 
-										   where u.userid='a';
-                                           
-insert into userrole  
-values ('a@a.aa' ,'2');
-commit;
